8921276772b09c0f0c4a365e297f749b6e0f8862
Beltelebot <60689861+beltelebot@users.noreply.github.com>
2021-03-09 22:33:49 +0300

    Update main.tf

--- 05_terraform_env_module/main.tf
+++ 05_terraform_env_module/main.tf
@@ -1,17 +1,6 @@
-module "dev_cluster" {
-  source = "./cluster"
-
-  cluster_name = "dev"
-}
-
 module "staging_cluster" {
   source = "./cluster"
 
   cluster_name = "staging"
 }
 
-module "production_cluster" {
-  source = "./cluster"
-
-  cluster_name = "production"
-}
