afde054e7e1d352f592c4eb46229bca174c9420d
Beltelebot <60689861+beltelebot@users.noreply.github.com>
2021-03-10 12:51:10 +0300

    Update main.tf

--- 05_terraform_env_module/cluster/main.tf
+++ 05_terraform_env_module/cluster/main.tf
@@ -66,7 +66,7 @@ resource "aws_iam_policy" "worker_policy" {
     host                   = data.aws_eks_cluster.cluster.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
     token                  = data.aws_eks_cluster_auth.cluster.token
-    load_config_file       = false
+ #   load_config_file       = false
   }
 }
 
