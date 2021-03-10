b6e33c85c0b883cfedbc1c1be6c8a1c4d033e043
Beltelebot <60689861+beltelebot@users.noreply.github.com>
2021-03-10 23:00:00 +0300

    Update main.tf

--- 05_terraform_env_module/cluster/main.tf
+++ 05_terraform_env_module/cluster/main.tf
@@ -1,8 +1,8 @@
 
 
-#variable "cluster_name" {
-#  default = "my-cluster"
-#}
+variable "cluster_name" {
+  default = "my-cluster"
+}
 
 
 data "aws_availability_zones" "available" {
@@ -97,10 +97,10 @@ provider "helm" {
     host                   = data.aws_eks_cluster.cluster.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
     token                  = base64decode(data.aws_eks_cluster_auth.cluster.token)
-#    load_config_file       = false
+    load_config_file       = false
   }
 }  
-
+ 
   
 resource "helm_release" "nginx_ingress" {
   depends_on = [null_resource.kubeconfig]   
