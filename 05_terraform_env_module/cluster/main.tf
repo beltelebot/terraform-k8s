821411743d2500100fd7bacea4036f0218f53c36
Beltelebot <60689861+beltelebot@users.noreply.github.com>
2021-03-10 16:22:49 +0300

    Update main.tf

--- 05_terraform_env_module/cluster/main.tf
+++ 05_terraform_env_module/cluster/main.tf
@@ -2,12 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-data "aws_availability_zones" "available" {}
 
-# Not required: currently used in conjunction with using
-# icanhazip.com to determine local workstation external IP
-# to open EC2 Security Group access to the Kubernetes cluster.
-# See workstation-external-ip.tf for additional information.
 provider "http" {}
 
 
