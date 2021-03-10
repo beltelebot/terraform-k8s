05f8c640e4e02a30bfcc8f29dc676fc0966e8efa
Beltelebot <60689861+beltelebot@users.noreply.github.com>
2021-03-10 17:23:47 +0300

    Update terraform.tf

--- 05_terraform_env_module/terraform.tf
+++ 05_terraform_env_module/terraform.tf
@@ -8,3 +8,26 @@ terraform {
   required_version = ">= 0.14"
 }
 
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+provider "http" {}
+
+
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+  
+ provider "helm" {
+  kubernetes {
+    config_path = "./kubeconfig_eks-staging"
+  }
+}
+
