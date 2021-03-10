40743e6a80669e19e0916deff4bc548ff0b9bc46
Beltelebot <60689861+beltelebot@users.noreply.github.com>
2021-03-10 15:17:28 +0300

    Update main.tf

--- 05_terraform_env_module/cluster/main.tf
+++ 05_terraform_env_module/cluster/main.tf
@@ -109,14 +109,3 @@ resource "helm_release" "nginx_ingress" {
   }
 }   
 
-resource "helm_release" "nginx_ingress" {
-  name       = "nginx-ingress-controller"
-
-  repository = "https://charts.bitnami.com/bitnami"
-  chart      = "nginx-ingress-controller"
-
-  set {
-    name  = "service.type"
-    value = "ClusterIP"
-  }
-}
