7d328e7819a3715b914ad7bc2c4f651b3e8586ec
Beltelebot <60689861+beltelebot@users.noreply.github.com>
2021-03-10 22:49:22 +0300

    Update terraform.tf

--- 05_terraform_env_module/terraform.tf
+++ 05_terraform_env_module/terraform.tf
@@ -1,14 +1,14 @@
 terraform {
+  required_version = ">= 0.14.3"
   required_providers {
-    aws = {
-     source  = "hashicorp/aws"
-     version = "~> 3.31.0"
+    helm = {
+      version = "=1.3.2"
+    }
+    null = {
+      version = ">=3.0.0"
     }
   }
-  required_version = ">= 0.14"
 }
-
-
 provider "aws" {
   region = "us-east-1"
 }
