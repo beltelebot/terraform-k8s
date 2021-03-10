06072b1b1b0a99d06348ba711197b03dd3bb3315
Beltelebot <60689861+beltelebot@users.noreply.github.com>
2021-03-10 17:33:24 +0300

    Update main.tf

--- 05_terraform_env_module/cluster/main.tf
+++ 05_terraform_env_module/cluster/main.tf
@@ -9,6 +9,22 @@ data "aws_availability_zones" "available" {
 }
 
 
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+  
+  
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
 #  version = "12.2.0"
