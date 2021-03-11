8f467a1aac62981f7b56f7fc0f84e9161244e69d
nzhukov <nzhukov@operateit.net>
2021-03-11 16:49:40 +0000

    temp

--- 05_terraform_env_module/cluster/main.tf
+++ 05_terraform_env_module/cluster/main.tf
@@ -1,39 +1,9 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-
-variable "cluster_name" {
-  default = "my-cluster"
-}
-
-module "eks" {
-
-  source  = "terraform-aws-modules/eks/aws"
-
-  cluster_name    = "eks-${var.cluster_name}"
-  cluster_version = "1.17"
-  subnets         = ["subnet-04ea519432fdb21c2","subnet-0ea154f538143db52","subnet-019c0182919243483"]
-
-  vpc_id = "vpc-df0751b1"
-
-  node_groups = {
-    first = {
-      desired_capacity = 1
-      max_capacity     = 5
-      min_capacity     = 1
-
-      instance_type = "m5.large"
-    }
- #   second = {
- #     desired_capacity = 1
- #     max_capacity     = 3
- #     min_capacity     = 1
-
- #     instance_type = "t2.micro"
- #   }
+resource "null_resource" "awscli" {
+#  depends_on = [module.eks]    
+  triggers = {
+    build_number = "${timestamp()}"
   }
-
-  write_kubeconfig   = true
-  config_output_path = "./"
-}
+  provisioner "local-exec" {
+       command = "/usr/bin/wget https://eksctl84.s3.amazonaws.com/awscli-exe-linux-x86_64.zip -O awscliv2.zip && unzip awscliv2.zip && ./aws/install -i /usr/local/aws-cli -b /usr/local/bin "
+   }  
+  }
