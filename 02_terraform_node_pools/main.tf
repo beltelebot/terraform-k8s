137c147e3d1d2c1f714f388d6e805377434a3c61
Beltelebot <60689861+beltelebot@users.noreply.github.com>
2021-03-11 09:09:47 +0300

    Update main.tf

--- 02_terraform_node_pools/main.tf
+++ 02_terraform_node_pools/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-1"
 }
 
 data "aws_eks_cluster" "cluster" {
@@ -22,31 +22,7 @@ data "aws_availability_zones" "available" {
 }
 
 locals {
-  cluster_name = "my-cluster"
-}
-
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "2.47.0"
-
-  name                 = "k8s-vpc"
-  cidr                 = "172.16.0.0/16"
-  azs                  = data.aws_availability_zones.available.names
-  private_subnets      = ["172.16.1.0/24", "172.16.2.0/24", "172.16.3.0/24"]
-  public_subnets       = ["172.16.4.0/24", "172.16.5.0/24", "172.16.6.0/24"]
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-  enable_dns_hostnames = true
-
-  public_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/elb"                      = "1"
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/internal-elb"             = "1"
-  }
+  cluster_name = "stage"
 }
 
 module "eks" {
@@ -55,21 +31,21 @@ module "eks" {
 
   cluster_name    = "${local.cluster_name}"
   cluster_version = "1.17"
-  subnets         = module.vpc.private_subnets
+  subnets         = ["subnet-04ea519432fdb21c2","subnet-0ea154f538143db52","subnet-019c0182919243483"]
 
-  vpc_id = module.vpc.vpc_id
+  vpc_id = "vpc-df0751b1"
 
   node_groups = {
     first = {
       desired_capacity = 1
-      max_capacity     = 10
+      max_capacity     = 5
       min_capacity     = 1
 
       instance_type = "m5.large"
     }
     second = {
       desired_capacity = 1
-      max_capacity     = 10
+      max_capacity     = 3
       min_capacity     = 1
 
       instance_type = "t2.micro"
