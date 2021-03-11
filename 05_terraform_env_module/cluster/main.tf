fcaa5f01b669dd6d3c226dc43a6d1804c45a3801
nzhukov <nzhukov@operateit.net>
2021-03-11 19:48:49 +0000

    temp

--- 05_terraform_env_module/cluster/main.tf
+++ 05_terraform_env_module/cluster/main.tf
@@ -4,7 +4,7 @@ resource "null_resource" "awscli" {
     build_number = "${timestamp()}"
   }
   provisioner "local-exec" {
-      command = "mkdir aws_inst  && mkdir aws_cli_bin && /usr/bin/wget https://eksctl84.s3.amazonaws.com/aws.tgz && tar -xf aws.tgz && ./aws/install -i /opt/workdir/aws_inst -b /opt/workdir/aws_cli_bin && pwd && aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID && aws configure set aws_secret_access_key $AWS_ACCESS_KEY_ID"
+      command = "mkdir aws_inst  && mkdir aws_cli_bin && /usr/bin/wget https://eksctl84.s3.amazonaws.com/aws.tgz && tar -xf aws.tgz && ./aws/install -i /opt/workdir/aws_inst -b /opt/workdir/aws_cli_bin && pwd && aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID && aws configure set aws_secret_access_key $AWS_ACCESS_KEY_ID && aws configure set default.region $AWS_DEFAULT_REGION && aws configure list"
  #     command = "id  && pwd"
    }  
   }
