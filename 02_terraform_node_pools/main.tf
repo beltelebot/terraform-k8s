2c7962b0ee82a6450d11ea0edc7432c7b7d4d7c9
nzhukov <nzhukov@operateit.net>
2021-03-12 07:24:15 +0000

    temp

--- 02_terraform_node_pools/main.tf
+++ 02_terraform_node_pools/main.tf
@@ -1,41 +1,41 @@
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
 }
 ##############################################
 
 
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
-}
+#data "aws_eks_cluster" "cluster" {
+#  name = module.eks.cluster_id
+#}
 
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
-}
+#data "aws_eks_cluster_auth" "cluster" {
+#  name = module.eks.cluster_id
+#}
 
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = false
-  version                = "~> 1.11"
-}
+#provider "kubernetes" {
+#  host                   = data.aws_eks_cluster.cluster.endpoint
+#  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+#  token                  = data.aws_eks_cluster_auth.cluster.token
+#  load_config_file       = false
+#  version                = "~> 1.11"
+#}
 
-data "aws_availability_zones" "available" {
-}
+#data "aws_availability_zones" "available" {
+#}
 
-locals {
-  cluster_name = "stage"
-}
+#locals {
+#  cluster_name = "stage"
+#}
 
 module "eks" {
 
   source  = "terraform-aws-modules/eks/aws"
 
-  cluster_name    = "${local.cluster_name}"
-  cluster_version = "1.17"
-  subnets         = ["subnet-04ea519432fdb21c2","subnet-0ea154f538143db52","subnet-019c0182919243483"]
+  cluster_name    = var.cluster_name
+  cluster_version = var.eks_version
+  subnets         = var.cluster_subnet_ids
 
-  vpc_id = "vpc-df0751b1"
+  vpc_id = var.vpc_id
 
   node_groups = {
     first = {
@@ -75,7 +75,7 @@ resource "null_resource" "kubectl_connect" {
     build_number = "${timestamp()}"
   }
   provisioner "local-exec" {
-      command = "aws eks --region us-east-1  update-kubeconfig --name stage"
+      command = "aws eks --region ${var.region}  update-kubeconfig --name ${var.cluster_name}"
    }  
   }
 
